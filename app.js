0e48033e4a47947040b5ef041b80d32b6674086a
Mouslim2004 <mouslimombadi@gmail.com>
2024-11-03 15:48:21 +0100

    Finish the front end of the homepage

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
+const express = require('express')// Framework to handle HTTP requests and define routes.
+const bodyParser = require('body-parser')//Middleware to handle and parse incoming request bodies (especially useful for POST and PUT requests).
+const mongoose = require('mongoose')//Connects to MongoDB and provides a structured way to interact with your database.
+const morgan = require('morgan')//Middleware for logging HTTP requests for debugging and monitoring purposes.
 const session = require('express-session')
 const flash = require('connect-flash')
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/lms')
+mongoose.connect('mongodb://localhost:27017/lms')//tells Mongoose to connect to a MongoDB database located 
+//at localhost on port 27017 and use the database named pushdb.
 
-const db = mongoose.connection
+const db = mongoose.connection //This line gets the default connection that Mongoose has established with MongoDB.
 db.on('error', () => {
   console.log('Connection to db failed!')
 })
@@ -16,8 +17,25 @@ db.once('open', () => {
   console.log('Connection to db success!')
 })
 
+
+app.set('view engine', 'ejs')//EJS is a templating engine that allows you to generate HTML with dynamic data.
+app.use(express.static('public'))//express.static('public') tells the app to look in the public folder for these files,
+//so when a client requests /css/style.css, it will automatically look for it in public/css/style.css.
+
+app.use(morgan('dev'))//Morgan logs details about incoming HTTP requests to the console,
+// such as the request method (GET, POST), the URL, response status, and response time.
+app.use(bodyParser.urlencoded({extended: false}))//This line tells the app to use the body-parser middleware to handle URL-encoded data (form submissions).
+//It allows the app to parse data from HTML forms (submitted via POST requests) and access it via req.body.
+app.use(bodyParser.json())//This line tells the app to use the body-parser middleware to handle JSON data.
+//If a client sends {"name": "John", "age": 30}, you can access req.body.name and req.body.age.
+
+app.get('/', (req,res) => {
+  res.render('home')
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
+  //This tells the Express application (app) to listen on a specific port for incoming HTTP requests.
 })
