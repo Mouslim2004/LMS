f6c59f1e200a03428371855bd9e5df0bfae93df1
Mouslim2004 <mouslimombadi@gmail.com>
2025-01-23 22:59:52 +0100

    Tried to inform user that book is borrowed

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -754,11 +754,11 @@ const adminGrantRequest = async (req,res) => {
       borrowedDate: new Date()
     })
 
-    // student.requestedBooks = student.requestedBooks.filter(
-    //   (reqBook) => reqBook.book.bookId.toString() !== bookId
-    // )
-
     await student.save()
+
+    const findBook = await Book.findOne({bookId : bookId})
+    await Book.findByIdAndUpdate(findBook._id, { $push: { borrowedBy: student._id} }, {new: true});
+    await Book.findByIdAndUpdate(findBook._id, {isBorrowed: true});
 
     console.log('Book granted successfully')
     res.status(200).json({ message: 'Book granted successfully' });
